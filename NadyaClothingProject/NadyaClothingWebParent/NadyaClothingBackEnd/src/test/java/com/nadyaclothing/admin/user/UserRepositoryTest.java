77326c2a48c4df3230951f5ea1c9876fa272e92c
cperera11 <chathuperera11@gmail.com>
2022-04-21 13:07:06 -0500

    resolved the warnings

--- NadyaClothingProject/NadyaClothingWebParent/NadyaClothingBackEnd/src/test/java/com/nadyaclothing/admin/user/UserRepositoryTest.java
+++ NadyaClothingProject/NadyaClothingWebParent/NadyaClothingBackEnd/src/test/java/com/nadyaclothing/admin/user/UserRepositoryTest.java
@@ -2,18 +2,12 @@ package com.nadyaclothing.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.awt.print.Pageable;
-import java.util.List;
-
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 
 import com.nadyaclothing.common.entity.Role;

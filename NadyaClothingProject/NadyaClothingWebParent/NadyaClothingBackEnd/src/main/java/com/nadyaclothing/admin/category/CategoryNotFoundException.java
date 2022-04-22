202fe9c814edab0e1a456acf830acf423674c9a7
cperera11 <chathuperera11@gmail.com>
2022-04-22 02:55:34 -0500

    Completed and running with Category

---
+++ NadyaClothingProject/NadyaClothingWebParent/NadyaClothingBackEnd/src/main/java/com/nadyaclothing/admin/category/CategoryNotFoundException.java
@@ -0,0 +1,14 @@
+package com.nadyaclothing.admin.category;
+
+public class CategoryNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CategoryNotFoundException(String message) {
+		super(message);
+	}
+
+}
